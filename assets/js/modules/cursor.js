650c917097102371108209508473459c2a70da34
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2021-07-19 18:31:15 -0300

    more updates

---
+++ assets/js/modules/cursor.js
@@ -0,0 +1,21 @@
+export default function initCursor(){
+    const cursor = document.getElementById('cursor');
+
+    function handlecursor(i){
+        let x = i.clientX;
+        let y = i.clientY;
+
+        cursor.style.top = y + "px";
+        cursor.style.left = x + "px";
+
+        if(x == 0 || y == 0 || x == window.innerWidth || y == window.innerHeight){
+            setTimeout(()=>{
+                cursor.classList.add('afk');
+            },2000)
+        }else{
+            cursor.classList.remove('afk');
+        }
+    }
+
+    document.addEventListener('mousemove', handlecursor);
+}
