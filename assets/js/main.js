b973ad1ff251f0fcd7c79e6925f593173cac739b
Leonardo Alencar <alencar.leonardo2020@gmail.com>
2021-07-24 22:13:00 -0300

    more updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-import initCursor from "./modules/cursor.js";
+//import initCursor from "./modules/cursor.js";
 import initNavBar from "./modules/navBar.js";
 import initSlider from "./modules/slider.js";
 
@@ -6,3 +6,29 @@ import initSlider from "./modules/slider.js";
 initNavBar();
 initSlider();
 // initCursor();
+
+const btn = document.getElementById('submit');
+
+function handleerror(event){
+    event.preventDefault();
+
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const message = document.getElementById('message');
+    const underlineName = document.querySelector('.position-name .underline');
+    const underlineEmail = document.querySelector('.position-email .underline');
+    const underlineMessage = document.querySelector('.position-message .underline');
+
+    if(name.value == ''){
+        underlineName.style.backgroundColor = 'red'
+    }
+    if(message.value == ''){
+        underlineMessage.style.backgroundColor = 'red'
+    }
+
+    if(email.value == '' || email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 || email.value.indexOf('.') - email.value.indexOf('@') == 1 || email.value.indexOf('@') == email.value.length || email.value.indexOf('.') == email.value.length ){
+        underlineEmail.style.backgroundColor = 'red'
+    }
+}
+
+btn.addEventListener('click', handleerror);
